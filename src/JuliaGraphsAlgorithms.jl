813eedbc412bd081759d1c8a3819b471a1cb8ddf
mromrom <moiromrom@alum.us.es>
2022-08-23 00:12:35 +0200

    first function added

--- src/JuliaGraphsAlgorithms.jl
+++ src/JuliaGraphsAlgorithms.jl
@@ -1,5 +1,9 @@
 module JuliaGraphsAlgorithms
 
 # Write your package code here..
+using Graphs
+using SimpleWeightedGraphs
 
+export reverse_delete_algorithm
+include("reverse_delete.jl")
 end
---
+++ src/reverse_delete.jl
@@ -0,0 +1,19 @@
+function reverse_delete_algorithm(initial_graph)
+    edges_initial_graph = collect(edges(initial_graph))
+    final_graph = SimpleWeightedGraph(nv(initial_graph))
+    digraph = SimpleDiGraph(initial_graph)
+    sort!(edges_initial_graph, rev=true, by = weight)
+    i = 1
+    while (i <= length(edges_initial_graph))
+        src = edges_initial_graph[i].src
+        dst = edges_initial_graph[i].dst
+        w = edges_initial_graph[i].weight
+        rem_edge!(digraph,src,dst)
+        if (!is_connected(digraph))
+            add_edge!(digraph, src, dst)
+            add_edge!(final_graph, src, dst, w)
+        end
+        i += 1
+    end
+    return final_graph
+end
